b60617bf8d97518d65f06fbeb65ce930f1f5f2eb
yashwanth kumar immadisetty <yashwanth.immadisetty@gmail.com>
2026-08-14 01:20:50 +0530

    Add vercel.json configuration

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)ifj^jk-c!p-fpy#hkf85y*i364s4x@@vb-q*&zj$z=j19^c$&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
